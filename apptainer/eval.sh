3afcd20aa8e7508b417d6c4347afe5134e8b69ed
Trzeciakiewicz <treciakiewicz@fe.hhi.de>
2023-03-13 17:10:17 +0100

    automatic push

--- apptainer/eval.sh
+++ apptainer/eval.sh
@@ -1,2 +1,2 @@
 cd /mnt/code
-python eval.py --data-path /mnt/datasets/cubicasa/  --log-path /mnt/output/job_results/ --weights weights/model_best_val_loss_var.pkl
+python eval.py --data-path /mnt/datasets/cubicasa/  --log-path /mnt/output/job_results/ --weights weights/model_best_val_loss_var_their.pkl
