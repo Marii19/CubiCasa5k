2ae576f8b01933a21ec1c93518ce1ed2864260e6
Trzeciakiewicz <treciakiewicz@fe.hhi.de>
2023-03-13 14:22:23 +0100

    automatic push

--- apptainer/train.sh
+++ apptainer/train.sh
@@ -1,3 +1,3 @@
 ls -l
 cd /mnt/code
-python train.py --data-path /mnt/datasets/cubicasa/ --n-epoch 1 --batch-size 80
+python train.py --data-path /mnt/datasets/cubicasa/ --n-epoch 1 --batch-size 80 --log-path /mnt/output/runs_cubi/
