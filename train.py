20180364d7db6bc5cf871b86a44925b29fb919d5
Trzeciakiewicz <treciakiewicz@fe.hhi.de>
2023-03-26 13:01:59 +0200

    automatic push

--- train.py
+++ train.py
@@ -158,12 +158,12 @@ def train(args, log_dir, writer, logger):
 
             file_path_image = os.path.join(out_numpy_dir, "image.npy")
             with open(file_path_image, "w") as f:
-                np.save(f, images.cpu())
+                np.save(f, np.array(images.cpu()))
 
 
             file_path_label = os.path.join(out_numpy_dir, "label.npy")
             with open(file_path_label, "w") as f:
-                np.save(f, labels.cpu())
+                np.save(f, np.array(labels.cpu()))
                 
 
             outputs = model(images)
